90221c479094004ac533591dbeb0269d2404bd6e
tousifws <tousif@wednesday.is>
2021-04-20 20:08:00 +0530

    updated

--- resources/v1/06_cars.sql
+++ resources/v1/06_cars.sql
@@ -4,8 +4,8 @@ CREATE TABLE cars
      model           VARCHAR (32) NOT NULL, 
      type            VARCHAR(32) NOT NULL, 
      status          VARCHAR(32) NOT NULL,
-     location        GEOMETRY NOT NULL, 
-     registrationNo  VARCHAR(32) NOT NULL,
-     user_id          INT NOT NULL
-     FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE 
+     location        GEOMETRY NOT NULL SRID 4326, 
+     registration_no  VARCHAR(32) NOT NULL UNIQUE,
+     user_id         INT NOT NULL
+   --   CONSTRAINT cars_user_id_fk FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE 
   ); 
--- resources/v1/07_bookings.sql
+++ resources/v1/07_bookings.sql
@@ -2,12 +2,11 @@ CREATE TABLE bookings
   ( 
      id              INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
      status          VARCHAR (32) NOT NULL, 
-     intial_location GEOMETRY NOT NULL, 
-     final_location  GEOMETRY NOT NULL, 
+     intial_location GEOMETRY NOT NULL SRID 4326, 
+     final_location  GEOMETRY NOT NULL SRID 4326, 
      amount          INT NOT NULL,
-     location        GEOMETRY NOT NULL, 
      user_id         INT NOT NULL,
      car_id          INT NOT NULL,
-     FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE,
+   --   FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE,
      FOREIGN KEY (car_id) REFERENCES cars (id) ON UPDATE CASCADE  
   ); 
